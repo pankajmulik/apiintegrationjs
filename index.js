0457151639468c1bfbd3fe9eeadd127b646998b7
pankajmulik <pankajmulik1997@gmail.com>
2024-03-30 13:55:07 +0530

    created one function to filter data by buttons

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
-    .then(res => {
+
+const url = "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
+
+fetch(url)
+    .then(
+        res => {
+            return res.json()
+        }
+    )
+    .then(
+        res => {
+            const loaddata = res;
+console.log(loaddata)
+
+            const button = document.querySelectorAll(".btn");
+
+            let selectedValue;
+            let product_data = ""
 
 
-        return res.json();
-    })
-    .then(data => {
+            loaddata.categories.map((category) => {
 
+             return   category.category_products.map((product) => {
 
-
-        const pdata = data
-        console.log(pdata)
-        let product_data = ""
-        pdata.categories.map((category) => {
-
-
-
-
-
-
-
-
-
-
-
-
-            category.category_products.map((product) => {
-
-                product_data +=
-                    `
+                 product_data +=
+                     `
 <div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
   box-shadow: 10px 15px 12px 10px gray;">
 
@@ -45,7 +41,7 @@ fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json'
     
     <div class="price">
         <p  style="text-align:center; font-weight:500;"> Price:  ${product.price
-                    }  INR
+                     }  INR
                     
                    
                     </p>
@@ -55,46 +51,40 @@ fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json'
 </div>
 
                 `
+            
+                    
+            
+                document.getElementById('products').innerHTML = product_data
+    
+             })
             })
-            document.getElementById('products').innerHTML = product_data
-
-        })
-
-
-    })
 
 
 
-function displayMen() {
-    fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
-        .then(res => {
 
 
-            return res.json();
-        })
-        .then(data => {
+            button.forEach(button => {
+                button.addEventListener("click", () => {
+                    selectedValue = button.getAttribute("data-value");
 
-            document.getElementById('products').innerHTML = ""
+                   let p_data=""
 
-            const pdata = data
-            console.log(pdata)
-            let product_data = ""
+                    document.getElementById('products').innerHTML = ""
 
-            const filtercate = pdata.categories.filter(category => category.category_name === "Men")
-            filtercate.map((category) => {
+                    const filtercate = loaddata.categories.filter(category => category.category_name === selectedValue)
+                    
+                    filtercate.map((category) => {
+                        category.category_products.map((product) =>{
+                            
 
-                console.log(category)
-
-
-
-                category.category_products.map((product) => {
-                    product_data +=
-                        `
-<div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
+                            p_data += `
+                           
+                           
+                            <div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
   box-shadow: 10px 15px 12px 10px gray;">
 
                   <div class="p-title" ">
-                  
+
                 <h4>
                 ${product.title}
                 </h4>
@@ -116,170 +106,20 @@ function displayMen() {
 
 </div>
 
-                `
+
+                            `
+
+                        })
+                        document.getElementById("products").innerHTML=p_data
+
+                    })
+
                 })
-                document.getElementById('products').innerHTML = product_data
-
-
-
-
-
-
-
-
-
-
-
             })
+           
+           
 
-        })
 
 
-}
-
-function displayWomen() {
-    fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
-        .then(res => {
-
-
-            return res.json();
-        })
-        .then(data => {
-
-            document.getElementById('products').innerHTML = ""
-
-            const pdata = data
-            console.log(pdata)
-            let product_data = ""
-
-            const filtercate = pdata.categories.filter(category => category.category_name === "Women")
-            filtercate.map((category) => {
-
-                console.log(category)
-
-
-
-                category.category_products.map((product) => {
-                    product_data +=
-                        `
-<div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
-  box-shadow: 10px 15px 12px 10px gray;">
-
-                  <div class="p-title" ">
-                  
-                <h4>
-                ${product.title}
-                </h4>
-
-                  </div>
-
-    <div class="prod-image">
-        <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
-    </div>
-    
-    <div class="price">
-        <p  style="text-align:center; font-weight:500;"> Price:  ${product.price
-                        }  INR
-                    
-                   
-                    </p>
-    </div>
-
-
-</div>
-
-                `
-                })
-                document.getElementById('products').innerHTML = product_data
-
-
-
-
-
-
-
-
-
-
-
-            })
-
-        })
-
-
-}
-
-function displayChild() {
-
-
-    fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
-        .then(res => {
-
-
-            return res.json();
-        })
-        .then(data => {
-
-            document.getElementById('products').innerHTML = ""
-
-            const pdata = data
-            console.log(pdata)
-            let product_data = ""
-
-            const filtercate = pdata.categories.filter(category => category.category_name === "Kids")
-            filtercate.map((category) => {
-
-                console.log(category)
-
-
-
-                category.category_products.map((product) => {
-                    product_data +=
-                        `
-<div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
-  box-shadow: 10px 15px 12px 10px gray;">
-
-                  <div class="p-title" ">
-                  
-                <h4>
-                ${product.title}
-                </h4>
-
-                  </div>
-
-    <div class="prod-image">
-        <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
-    </div>
-    
-    <div class="price">
-        <p  style="text-align:center; font-weight:500;"> Price:  ${product.price
-                        }  INR
-                    
-                   
-                    </p>
-    </div>
-
-
-</div>
-
-                `
-                })
-                document.getElementById('products').innerHTML = product_data
-
-
-
-
-
-
-
-
-
-
-
-            })
-
-        })
-
-
-
-}
+        }
+    )
