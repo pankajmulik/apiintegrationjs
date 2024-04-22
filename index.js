9c30955efc5563d1936a3b72c094cf03f7804abe
PANKAJ MULIK <pankajmulik1997@gmail.com>
2024-04-22 22:44:30 +0530

    search input

--- index.js
+++ index.js
@@ -10,78 +10,128 @@ fetch(url)
     .then(
         res => {
             const loaddata = res;
-console.log(loaddata)
+            console.log(loaddata)
 
             const button = document.querySelectorAll(".btn");
 
-            let selectedValue;
-            let product_data = ""
 
+            const searchInput = document.getElementById("searchbar")
 
+let disdata=''
             loaddata.categories.map((category) => {
+                category.category_products.map((product) => {
+                    
 
-             return   category.category_products.map((product) => {
-
-                 product_data +=
-                     `
-<div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
-  box-shadow: 10px 15px 12px 10px gray;">
+                    disdata += `
+                           
+                           
+                            <div class="pdata"
+                            style="margin:10px; background-color:white
+                             text-center;  box-sizing: border-box;
+                            box-shadow: 10px
+                             15px 12px 10px gray;">
 
                   <div class="p-title" ">
-                  
+
                 <h4>
                 ${product.title}
                 </h4>
 
                   </div>
 
-    <div class="prod-image">
-        <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
-    </div>
+                 <div class="prod-image">
+                 <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
+                 </div>
     
-    <div class="price">
-        <p  style="text-align:center; font-weight:500;"> Price:  ${product.price
-                     }  INR
+                  <div class="price">
+              <p  style="text-align:center; font-weight:500;">
+              Price:  ${product.price
+                        }  INR
                     
                    
                     </p>
-    </div>
+                  </div>
+
+                <div>
+                <button>Buy</button>
+                <button class="cart-btn"> Add to Cart</button>
+
+                </div>
 
 
-</div>
+                   </div>
 
-                `
-            
+
+                            `
                     
-            
-                document.getElementById('products').innerHTML = product_data
-    
-             })
+
+
+                })
+                document.getElementById("products").innerHTML=disdata
             })
-
-
+            
 
 
 
             button.forEach(button => {
                 button.addEventListener("click", () => {
-                    selectedValue = button.getAttribute("data-value");
+                    const selectedValue = button.getAttribute("data-value");
 
-                   let p_data=""
+
 
                     document.getElementById('products').innerHTML = ""
 
-                    const filtercate = loaddata.categories.filter(category => category.category_name === selectedValue)
-                    
-                    filtercate.map((category) => {
-                        category.category_products.map((product) =>{
-                            
 
-                            p_data += `
+
+
+                    const filtercate = loaddata.categories.filter(category => category.category_name === selectedValue)
+                    filtercate.map((category) => {
+                        displaydata(category)
+
+                    })
+
+
+
+                })
+
+            })
+
+
+            let cart=[]
+
+            const cartbtn = document.querySelectorAll(".cart-btn")
+            
+            cartbtn.forEach(btn => {
+                btn.addEventListener("click", () => {
+                    loaddata.categories.map((category) => {
+                        category.filter((category) => {
+                            
+                        })
+                    })
+                })
+            })
+
+
+
+
+
+
+
+            function displaydata(category) {
+                let p_data = ""
+                
+
+                category.category_products.map((product) => {
+
+
+                    p_data += `
                            
                            
-                            <div class="pdata" style="margin:10px; background-color:white text-center;  box-sizing: border-box;
-  box-shadow: 10px 15px 12px 10px gray;">
+                            <div class="pdata"
+                            style="margin:10px; background-color:white
+                             text-center;  box-sizing: border-box;
+                            box-shadow: 10px
+                             15px 12px 10px gray;">
 
                   <div class="p-title" ">
 
@@ -91,35 +141,87 @@ console.log(loaddata)
 
                   </div>
 
-    <div class="prod-image">
-        <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
-    </div>
+                 <div class="prod-image">
+                 <img src=${product.image} alt="product image" style="width:300px; height:300px; onject-fit:contain">
+                 </div>
     
-    <div class="price">
-        <p  style="text-align:center; font-weight:500;"> Price:  ${product.price
+                  <div class="price">
+              <p  style="text-align:center; font-weight:500;">
+              Price:  ${product.price
                         }  INR
                     
                    
                     </p>
-    </div>
+                  </div>
+
+                <div>
+                <button>Buy</button>
+                <button class="cart-btn"> Add to Cart</button>
+
+                </div>
 
 
-</div>
+                   </div>
 
 
                             `
 
-                        })
-                        document.getElementById("products").innerHTML=p_data
+                })
+                document.getElementById("products").innerHTML = p_data
+
+
+            }
+
+
+
+
+            searchInput.addEventListener("input", () => {
+
+                const selectedValue = searchInput.value.toLowerCase()
+
+                document.getElementById('products').innerHTML = ""
+
+
+                let loading = false
+
+
+                if (loaddata.categories.filter(category => category.category_name === selectedValue)) {
+
+                    const filtercate = loaddata.categories.filter(category => category.category_name.toLowerCase() === selectedValue)
+                    filtercate.map((category) => {
+                        displaydata(category)
 
                     })
+                    loading = true
 
-                })
+                }
+
+                if (!loading) {
+
+                    const vendorfilter = loaddata.categories.map((category) => {
+                        category.category_products.filter(cat => selectedValue.includes(cat.title.toLowerCase()))
+                    })
+
+
+                }
+
+
+
+
+
+
+
+
+
+
             })
-           
-           
+
+
 
 
 
         }
     )
+
+
+
